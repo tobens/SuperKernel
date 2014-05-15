952196c5f82b9b2d45c6b8316fecc81830ce2cb9
Al Viro <viro@zeniv.linux.org.uk>
2014-05-15 05:06:42 -0400

    vm_area_operations: kill ->migrate()
    the only instance this method has ever grown was one in kernfs -
    one that call ->migrate() of another vm_ops if it exists.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -294,8 +294,6 @@ struct vm_operations_struct {
 	 */
 	struct mempolicy *(*get_policy)(struct vm_area_struct *vma,
 					unsigned long addr);
-	int (*migrate)(struct vm_area_struct *vma, const nodemask_t *from,
-		const nodemask_t *to, unsigned long flags);
 #endif
 
 	/*
