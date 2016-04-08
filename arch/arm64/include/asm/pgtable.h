2260aa5b8d53f28a60249bfd041dbd95cb871397
Ganapatrao Kulkarni <gkulkarni@caviumnetworks.com>
2016-04-08 15:50:28 -0700

    arm64, mm, numa: Add NUMA balancing support for arm64.
    Enable NUMA balancing for arm64 platforms.
    Add pte, pmd protnone helpers for use by automatic NUMA balancing.
    
    Reviewed-by: Steve Capper <steve.capper@arm.com>
    Reviewed-by: Robert Richter <rrichter@cavium.com>
    Signed-off-by: Ganapatrao Kulkarni <gkulkarni@caviumnetworks.com>
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -267,6 +267,21 @@ static inline pmd_t pte_pmd(pte_t pte)
 	return __pmd(pte_val(pte));
 }
 
+#ifdef CONFIG_NUMA_BALANCING
+/*
+ * See the comment in include/asm-generic/pgtable.h
+ */
+static inline int pte_protnone(pte_t pte)
+{
+	return (pte_val(pte) & (PTE_VALID | PTE_PROT_NONE)) == PTE_PROT_NONE;
+}
+
+static inline int pmd_protnone(pmd_t pmd)
+{
+	return pte_protnone(pmd_pte(pmd));
+}
+#endif
+
 /*
  * THP definitions.
  */
